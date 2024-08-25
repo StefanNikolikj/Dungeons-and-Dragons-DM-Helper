8833ffc457756e0f7e04bc11c2aad3d9cac2874b
Nikolikj Stefan <nikolik.stefan@gmail.com>
2024-08-25 23:51:25 +0200

    Entity statistics

--- Dungeons and Dragons DM Helper/Combatant.cs
+++ Dungeons and Dragons DM Helper/Combatant.cs
@@ -18,8 +18,14 @@ namespace Dungeons_and_Dragons_DM_Helper
         public int hp;
         public int AC;
         public int proficiency;
+        public List<int> savingThrows = new List<int> ();
+        public List<int> damageResistances = new List<int> ();
+        public List<int> damageImmunities = new List<int> ();
+        public List<int> damageVulnerabilities = new List<int> ();
+        public List<int> conditionImmunities = new List<int> () ;
 
-        public Combatant(string name, int strenght, int dexterity, int constitution, int intelligence, int wisdom, int charisma, int hp, int ac, int proficiency)
+        public Combatant(string name, int strenght, int dexterity, int constitution, int intelligence, int wisdom, int charisma, int hp, int ac, int proficiency, List<int> damageResistances, List<int> damageImmunities, List<int> damageVulnerabilities, List<int> savingThrows)
+
         {
             this.name = name;
             this.strenght = strenght;
@@ -31,11 +37,21 @@ namespace Dungeons_and_Dragons_DM_Helper
             this.hp = hp;
             this.AC = ac;
             this.proficiency = proficiency;
+            this.damageResistances = damageResistances;
+            this.damageImmunities = damageImmunities;
+            this.damageVulnerabilities = damageVulnerabilities;
+            this.savingThrows = savingThrows;
         }
 
         public int modifierCalc(int stat)
         {
             return stat / 2 - 5;
         }
+
+        List<String> damageTypes = new List<String>() {"Piercing", "Bludgeoning", "Slashing", "Cold", "Fire", "Lightning", "Thunder", "Poison", "Acid", "Necrotic", "Radiant", "Force", "Psychic"};
+
+        List<String> conditions = new List<String>() {"Blinded", "Charmed", "Deafened", "Frightened", "Grappled", "Incapacitated", "Invisible", "Paralyzed", "Petrified", "Poisoned", "Prone", "Restrained", "Stunned", "Unconscious"};
+
+
     }
 }
